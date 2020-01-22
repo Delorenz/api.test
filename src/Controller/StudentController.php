0aea2674783f7a59e4daa021c4cc666c84188758
DE LORENZI Lucas <ldeloren@icn-artem.com>
2020-01-22 12:11:15 +0100

    first commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Student;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/students", name="students")
+     */
+    public function index()
+    {
+
+        $students = $this->getDoctrine()
+        ->getRepository(Student::class)
+        ->findAll();
+       
+        foreach($students as $student) {
+            $arrayCollection[] = array(
+                'uid' => $student->getUid(),
+               'username' => $student->getUsername(),
+               'first_name' => $student->getFirstName(),
+               'last_name' => $student->getLastName(),
+               'mail' => $student->getMail(),
+               'promo' => $student->getPromo()
+            );
+       }
+       
+
+        if (!$students) {
+            throw $this->createNotFoundException(
+                'No students found'
+            );
+        }
+        
+       return new JsonResponse( $arrayCollection);
+      
+       
+    }
+
+    /**
+     * @Route("/student/get/{id}", name="get student")
+     */
+    public function getStudent($id)
+    {
+
+        $student = $this->getDoctrine()
+        ->getRepository(Student::class)
+        ->findOneBy(array('uid' => $id));
+       
+        
+
+        if (!$student) {
+            throw $this->createNotFoundException(
+                'No student found'
+            );
+        }
+
+        $response = array( 'uid' => $student->getUid(),
+        'username' => $student->getUsername(),
+        'first_name' => $student->getFirstName(),
+        'last_name' => $student->getLastName(),
+        'mail' => $student->getMail(),
+        'promo' => $student->getPromo());
+        
+       return new JsonResponse( $response);
+      
+       
+    }
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StudentRepository")
+ */
+class Student
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $promo;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $uid;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(?string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(?string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getPromo(): ?string
+    {
+        return $this->promo;
+    }
+
+    public function setPromo(?string $promo): self
+    {
+        $this->promo = $promo;
+
+        return $this;
+    }
+
+    public function getUid(): ?int
+    {
+        return $this->uid;
+    }
+
+    public function setUid(int $uid): self
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+}
